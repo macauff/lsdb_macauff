fac1f4ed98039b344d9a1c2b89ea5e0dde0a1c16
Melissa DeLucchi <delucchi@andrew.cmu.edu>
2024-06-12 12:24:54 -0400

    Re-arrange resume functionality for recent import changes.

--- src/lsdb_macauff/import_pipeline/resume_plan.py
+++ src/lsdb_macauff/import_pipeline/resume_plan.py
@@ -2,16 +2,19 @@
 
 from __future__ import annotations
 
+import pickle
 from dataclasses import dataclass, field
-from typing import List, Tuple
+from typing import List
 
+import healpy as hp
+import numpy as np
+from hipscat.catalog import Catalog
 from hipscat.io import FilePointer, file_io
 from hipscat.pixel_math.healpix_pixel import HealpixPixel
 from hipscat_import.pipeline_resume_plan import PipelineResumePlan
+from tqdm.auto import tqdm
 
 from lsdb_macauff.import_pipeline.arguments import MacauffArguments
-
-# pylint: disable=duplicate-code
 
 
 @dataclass
@@ -20,51 +23,99 @@ class MacauffResumePlan(PipelineResumePlan):
 
     input_paths: List[FilePointer] = field(default_factory=list)
     """resolved list of all files that will be used in the importer"""
-    split_keys: List[Tuple[str, str]] = field(default_factory=list)
-    """set of files (and job keys) that have yet to be split"""
-    reduce_keys: List[Tuple[HealpixPixel, str]] = field(default_factory=list)
-    """set of left side catalog pixels (and job keys) that have yet to be reduced/combined"""
+    left_pixels: List[HealpixPixel] = field(default_factory=list)
+    highest_left_order: int = 0
+    highest_right_order: int = 0
+    left_alignment_file: str = None
+    right_alignment_file: str = None
 
     SPLITTING_STAGE = "splitting"
     REDUCING_STAGE = "reducing"
+    LEFT_ALIGNMENT_FILE = "left_alignment.pickle"
+    RIGHT_ALIGNMENT_FILE = "right_alignment.pickle"
 
-    def __init__(self, args: MacauffArguments, left_pixels):
+    def __init__(self, args: MacauffArguments):
         if not args.tmp_path:  # pragma: no cover (not reachable, but required for mypy)
             raise ValueError("tmp_path is required")
         super().__init__(resume=args.resume, progress_bar=args.progress_bar, tmp_path=args.tmp_path)
         self.input_paths = args.input_paths
-        self.gather_plan(left_pixels)
+        self.gather_plan(args)
 
-    def gather_plan(self, left_pixels):
+    def gather_plan(self, args):
         """Initialize the plan."""
         ## Make sure it's safe to use existing resume state.
         super().safe_to_resume()
 
-        ## Validate existing resume state.
-        ## - if a later stage is complete, the earlier stages should be complete too.
-        splitting_done = self.is_splitting_done()
-        reducing_done = self.is_reducing_done()
+        with tqdm(total=5, desc="Planning", disable=not args.progress_bar) as step_progress:
+            ## Validate existing resume state.
+            ## - if a later stage is complete, the earlier stages should be complete too.
+            splitting_done = self.is_splitting_done()
+            reducing_done = self.is_reducing_done()
 
-        if reducing_done and not splitting_done:
-            raise ValueError("splitting must be complete before reducing")
+            if reducing_done and not splitting_done:
+                raise ValueError("splitting must be complete before reducing")
 
-        ## Validate that we're operating on the same file set as the previous instance.
-        self.input_paths = self.check_original_input_paths(self.input_paths)
+            ## Validate that we're operating on the same file set as the previous instance.
+            self.input_paths = self.check_original_input_paths(self.input_paths)
 
-        ## Gather keys for execution.
-        if not splitting_done:
-            self.split_keys = self.get_remaining_split_keys()
-        if not reducing_done:
-            self.reduce_keys = self.get_reduce_keys(left_pixels)
-        ## Go ahead and create our directories for storing resume files.
-        file_io.make_directory(
-            file_io.append_paths_to_pointer(self.tmp_path, self.SPLITTING_STAGE),
-            exist_ok=True,
-        )
-        file_io.make_directory(
-            file_io.append_paths_to_pointer(self.tmp_path, self.REDUCING_STAGE),
-            exist_ok=True,
-        )
+            left_catalog = Catalog.read_from_hipscat(args.left_catalog_dir)
+            step_progress.update(1)
+            right_catalog = Catalog.read_from_hipscat(args.right_catalog_dir)
+            step_progress.update(1)
+            self.highest_left_order = left_catalog.partition_info.get_highest_order()
+            self.highest_right_order = right_catalog.partition_info.get_highest_order()
+
+            self.left_pixels = left_catalog.partition_info.get_healpix_pixels()
+            right_pixels = right_catalog.partition_info.get_healpix_pixels()
+
+            ## Gather keys for execution.
+            if not splitting_done:
+                self.left_alignment_file = file_io.append_paths_to_pointer(
+                    self.tmp_path, self.LEFT_ALIGNMENT_FILE
+                )
+                self.right_alignment_file = file_io.append_paths_to_pointer(
+                    self.tmp_path, self.RIGHT_ALIGNMENT_FILE
+                )
+                if not file_io.does_file_or_directory_exist(
+                    self.left_alignment_file
+                ) or not file_io.does_file_or_directory_exist(self.right_alignment_file):
+                    regenerated_left_alignment = np.full(hp.order2npix(self.highest_left_order), None)
+                    for pixel in self.left_pixels:
+                        explosion_factor = 4 ** (self.highest_left_order - pixel.order)
+                        exploded_pixels = np.arange(
+                            pixel.pixel * explosion_factor,
+                            (pixel.pixel + 1) * explosion_factor,
+                        )
+                        regenerated_left_alignment[exploded_pixels] = pixel
+                    regenerated_right_alignment = np.full(hp.order2npix(self.highest_right_order), None)
+                    for pixel in right_pixels:
+                        explosion_factor = 4 ** (self.highest_right_order - pixel.order)
+                        exploded_pixels = np.arange(
+                            pixel.pixel * explosion_factor,
+                            (pixel.pixel + 1) * explosion_factor,
+                        )
+                        regenerated_right_alignment[exploded_pixels] = pixel
+
+                    with open(self.left_alignment_file, "wb") as pickle_file:
+                        pickle.dump(regenerated_left_alignment, pickle_file)
+
+                    with open(self.right_alignment_file, "wb") as pickle_file:
+                        pickle.dump(regenerated_right_alignment, pickle_file)
+
+            step_progress.update(1)
+
+            if not reducing_done:
+                self.reduce_keys = self.get_reduce_keys()
+            ## Go ahead and create our directories for storing resume files.
+            file_io.make_directory(
+                file_io.append_paths_to_pointer(self.tmp_path, self.SPLITTING_STAGE),
+                exist_ok=True,
+            )
+            file_io.make_directory(
+                file_io.append_paths_to_pointer(self.tmp_path, self.REDUCING_STAGE),
+                exist_ok=True,
+            )
+            step_progress.update(1)
 
     def get_remaining_split_keys(self):
         """Gather remaining keys, dropping successful split tasks from done file names.
@@ -111,7 +162,7 @@ class MacauffResumePlan(PipelineResumePlan):
         """Are there files left to split?"""
         return self.done_file_exists(self.SPLITTING_STAGE)
 
-    def get_reduce_keys(self, left_pixels):
+    def get_reduce_keys(self):
         """Fetch a Tuple for each partition to reduce.
 
         Tuple contains:
@@ -123,7 +174,7 @@ class MacauffResumePlan(PipelineResumePlan):
         reduced_keys = set(self.read_done_keys(self.REDUCING_STAGE))
         reduce_items = [
             (hp_pixel, f"{hp_pixel.order}_{hp_pixel.pixel}")
-            for hp_pixel in left_pixels
+            for hp_pixel in self.left_pixels
             if f"{hp_pixel.order}_{hp_pixel.pixel}" not in reduced_keys
         ]
         return reduce_items
@@ -132,10 +183,10 @@ class MacauffResumePlan(PipelineResumePlan):
         """Are there partitions left to reduce?"""
         return self.done_file_exists(self.REDUCING_STAGE)
 
-    def wait_for_reducing(self, futures, left_pixels):
+    def wait_for_reducing(self, futures):
         """Wait for reducing futures to complete."""
         self.wait_for_futures(futures, self.REDUCING_STAGE)
-        remaining_reduce_items = self.get_reduce_keys(left_pixels)
+        remaining_reduce_items = self.get_reduce_keys()
         if len(remaining_reduce_items) > 0:
             raise RuntimeError(f"{len(remaining_reduce_items)} reduce stages did not complete successfully.")
         self.touch_stage_done_file(self.REDUCING_STAGE)
